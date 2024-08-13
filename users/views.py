83d2bd47782d8320858cb6693e12398c46516fbe
alisher <dautovalisher33@gmail.com>
2024-08-13 22:11:39 +0500

    Add for method POST form save

--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:list')
+
+
     form = UserCreationForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
