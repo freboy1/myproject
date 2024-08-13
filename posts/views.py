848e66c29c196ffa753795cd6674a5483947db91
alisher <dautovalisher33@gmail.com>
2024-08-13 15:31:28 +0500

    Add post_page view return HttpResponse

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     context = {'posts': posts}
     return render(request, 'posts/posts_list.html', context)
+
+def post_page(request, slug):
+    return HttpResponse(slug)
