7b67cb9338fab2e901c50d36f3ec9015a083a738
alisher <dautovalisher33@gmail.com>
2024-08-13 23:15:06 +0500

    Add post_new view & login_required

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
@@ -11,3 +12,7 @@ def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     context = {'post': post}
     return render(request, 'posts/post_page.html', context)
+
+@login_required(login_url="users/login")
+def post_new(request):
+    return render(request, 'posts/post_new.html')
