fc39cdd50594b1eebf3d143f3b8d0a8b369e8547
alisher <dautovalisher33@gmail.com>
2024-08-13 22:46:35 +0500

    Add my_login view

--- users/views.py
+++ users/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 def register(request):
-
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('posts:list')
-
-
     form = UserCreationForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+def my_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, user=form.get_user())
+            return redirect('posts:list')
+    form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
