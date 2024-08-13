de9dd7db086d8b9a16ecbab56426d083e85c3cbb
alisher <dautovalisher33@gmail.com>
2024-08-13 21:57:43 +0500

    Add UserCreationForm to context

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(request):
-    return render(request, 'users/register.html')
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
