26870c36dece3d34b3d34ae13868e80907a7774c
alisher <dautovalisher33@gmail.com>
2024-08-13 23:04:09 +0500

    Add csrf-token & url logout

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name='users'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.my_login, name='login'),
+    path('logout/', views.my_logout, name='logout'),
 ]
